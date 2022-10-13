123a473dfad8414740d58002515c13b176de19ec
Alexander Ore <aore8030@gmail.com>
2022-10-13 20:23:37 +0100

    Updating construction

--- web/admin.py
+++ web/admin.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for, redirect, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 admin = Blueprint('admin', import_name='admin', url_prefix='/')
 
@@ -6,3 +7,12 @@ admin = Blueprint('admin', import_name='admin', url_prefix='/')
 @admin.route('/admin')
 def dash():
     return 'Admin'
+
+
+@admin.route('/admin-login', methods=['POST', 'GET'])
+def dash_login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+    return render_template('admin/login.html')
